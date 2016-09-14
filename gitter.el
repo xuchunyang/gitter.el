2dd9618657a0aa565ad840ff028406351da86fcb
Chunyang Xu <xuchunyang.me@gmail.com>
2016-09-14 19:09:25 +0800

    Add documentation

--- gitter.el
+++ gitter.el
@@ -23,7 +23,133 @@
 
 ;;; Commentary:
 
-;;
+;;                              _____________
+
+;;                                GITTER.EL
+
+;;                               Chunyang Xu
+;;                              _____________
+
+
+;; Table of Contents
+;; _________________
+
+;; 1 Prerequisites
+;; 2 Setup
+;; 3 Usage
+;; 4 Customization
+;; .. 4.1 Emoji
+;; 5 Limitation
+;; 6 To do
+
+
+;; [[https://badges.gitter.im/M-x-Gitter/Lobby.svg]]
+
+;; A rough [Gitter] client for [GNU Emacs].
+
+
+;; [[https://badges.gitter.im/M-x-Gitter/Lobby.svg]]
+;; https://gitter.im/M-x-Gitter/Lobby
+
+;; [Gitter]
+;; https://gitter.zendesk.com/hc/en-us/articles/200178981-What-s-Gitter-written-in-
+
+;; [GNU Emacs] https://www.gnu.org/software/emacs/
+
+
+;; 1 Prerequisites
+;; ===============
+
+;;   - cURL
+;;   - Emacs 24.1 or newer
+
+
+;; 2 Setup
+;; =======
+
+;;   Add gitter.el to your `load-path' and require. Something like:
+
+;;   (add-to-list 'load-path "path/to/gitter.el/")
+;;   (require 'gitter)
+
+;;   Alternatively, if you manage loading packages with use-package:
+
+;;   (use-package gitter
+;;     :load-path "path/to/gitter.el/"
+;;     :defer t
+;;     :commands gitter)
+
+;;   Then you will need to set the authentication-token. Follow these steps
+;;   to get your token:
+;;   1) Visit URL `[https://developer.gitter.im]'
+;;   2) Click Sign in (top right)
+;;   3) You will see your personal access token at URL
+;;      `[https://developer.gitter.im/apps]'
+
+;;   When you save this variable, DON'T WRITE IT ANYWHERE PUBLIC.
+
+;;   (setq gitter-token "your-token")
+
+
+;; 3 Usage
+;; =======
+
+;;   Type `M-x gitter' to join a romm and start chatting.
+
+
+;; 4 Customization
+;; ===============
+
+;; 4.1 Emoji
+;; ~~~~~~~~~
+
+;;   If you want to display Emoji, install [emojify] *or*
+;;   [emoji-cheat-sheet-plus] *or* [company-emoji], they are all avaiable
+;;   from MELPA. You only need to install one of them, but if you install
+;;   more than one of them, the priority is emojify >
+;;   emoji-cheat-sheet-plus > company-emoji.
+
+
+;; [emojify] https://github.com/iqbalansari/emacs-emojify
+
+;; [emoji-cheat-sheet-plus]
+;; https://github.com/syl20bnr/emacs-emoji-cheat-sheet-plus
+
+;; [company-emoji] https://github.com/dunn/company-emoji
+
+
+;; 5 Limitation
+;; ============
+
+;;   If you are a serious Gitter user (I am not) and you compare this
+;;   little program with other official Gitter clients, I guess you will
+;;   probably be very disappointed: lack of functions and features, buggy
+;;   etc, so now you have been warnned. However, feedback, suggestion and
+;;   patch are always welcome.
+
+;;   By the way, Gitter provides [IRC access] and there are several
+;;   well-known IRC clients for Emacs.
+
+
+;; [IRC access] https://irc.gitter.im/
+
+
+;; 6 To do
+;; =======
+
+;;   - [ ] Markup message
+;;     - plain link
+;;     - Markdown link
+;;     - Github-flavored Markdown image
+;;     - @mention
+;;     - Markdown inline code block (add syntax highlighting if it is
+;;       possible and very easy)
+;;     - Github-flavored fenced code block (add syntax highlighting if it
+;;       has proper language tag)
+;;     - Github-flavored indented with four spaces code block (prefer no
+;;       syntax highlighting to avoiding guessing what language the code is
+;;       in, I don't like guess)
+;;     - etc
 
 ;;; Code:
 
